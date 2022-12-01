284fdfe4e08e90722b250c44b05b3ecbb306dc3a
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-12-01 16:17:46 +0200

    [TASK 2] check last digit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+denom = 10 if number >= 0 else -10
+if number % denom > 5:
+    print(f"Last digit of {number:d} is {number % denom} and is greater than 5")
+elif number % denom < 6 and number != 0:
+    print(f"Last digit of {number:d} is {number % denom} and is less than 6 and not 0")
+else:
+    print(f"Last digit of {number:d} is {number % denom} and is 0")
