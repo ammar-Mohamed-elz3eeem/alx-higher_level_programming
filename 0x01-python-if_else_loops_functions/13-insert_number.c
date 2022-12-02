7d72cd3e8f5fbe53619ade6f254ea7722c60264d
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-12-02 20:52:30 +0200

    [TASK 14][FIX] remove printf

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -36,7 +36,6 @@ listint_t *insert_node(listint_t **head, int number)
 		if (node->n > number)
 		{
 			curr->n = number;
-			printf("curr->n = %d\n", curr->n);
 			old->next = curr;
 			curr->next = node;
 			break;
