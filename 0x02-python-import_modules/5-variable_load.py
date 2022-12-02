a950c5a938c40e2807eeff2935e14ca12357c63a
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-12-02 13:40:00 +0200

    [TASK 6][FIX] change module name

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import calculator_1
+    import variable_load_5
 
-    print("{}".format(calculator_1.a))
+    print("{}".format(variable_load_5.a))
