ca22f139ad470e97d8d0cd9306bd9a0b91a775d6
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-12-03 02:44:41 +0200

    [TASK 7] simple calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
+    argv = argv[1:]
+    if len(argv) != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    if argv[1] not in "+-*/":
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    op = argv[1]
+    a = int(argv[0])
+    b = int(argv[2])
+    if op == '+':
+        res = add(a, b)
+    elif op == '-':
+        res = sub(a, b)
+    elif op == '*':
+        res = mul(a, b)
+    else:
+        res = div(a, b)
+    
+    print("{} {:s} {} = {}".format(argv[0], op, argv[2], res))
