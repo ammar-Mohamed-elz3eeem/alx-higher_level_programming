12d092ba36649bf0f7d5fa4f99d4407705c0d1a4
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-12-07 04:09:10 +0200

    [FIX] pycodestyle

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
     newstr = ""
-    
-    if str == None:
+
+    if str is None:
         return None
-    
+
     for i in my_string:
         if i == 'c' or i == 'C':
             continue
