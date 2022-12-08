31fab5f882a8a0183c1ce619c18306fb5a03d1db
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-12-08 12:06:04 +0200

    [FIX] re-init tups

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,37 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    first = 0
-    second = 0
-    lentupa = len(tuple_a)
-    lentupb = len(tuple_b)
-    if (lentupa == 0):
-        first = tuple_b[0]
-        second = tuple_b[1]
-        return (first, second)
-    if (lentupb == 0):
-        first = tuple_a[0]
-        second = tuple_a[1]
-        return (first, second)
-    if (lentupa == 1):
-        first = tuple_a[0]
-        second = 0
-        if (lentupb == 1):
-            first += tuple_b[0]
-            second += 0
-        else:
-            first += tuple_b[0]
-            second += tuple_b[1]
-        return first, second
-    if (lentupb == 1):
-        first = tuple_b[0]
-        second = 0
-        if (lentupa == 1):
-            first += tuple_a[0]
-            second += 0
-        else:
-            first += tuple_a[0]
-            second += tuple_a[1]
-        return first, second
+    tuple_a = tuple_a + (0, 0)
+    tuple_b = tuple_b + (0, 0)
     first = tuple_a[0] + tuple_b[0]
-    second = tuple_b[1] + tuple_a[1]
+    second = tuple_a[1] + tuple_b[1]
     return (first, second)
