f48cf314ae5d7cea9bbf7befd0e2a937a927227c
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-12-09 21:25:20 +0200

    [FIX] only element test case

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None: 
+    if a_dictionary is None:
         return None
     i = 0
     best_score = 0
+    winner = None
     for key, val in a_dictionary.items():
-        if i == 0: best_score = val
-        if best_score < val: 
+        if i == 0:
             best_score = val
             winner = key
-        i+=1
+        if best_score < val:
+            best_score = val
+            winner = key
+        i += 1
     return winner
