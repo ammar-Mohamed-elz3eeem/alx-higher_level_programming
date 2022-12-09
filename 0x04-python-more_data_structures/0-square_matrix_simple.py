a3cd765927c079fa2bee25da6fd6dc94ce6d3d56
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-12-09 19:37:28 +0200

    [FIX] remove test code

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,12 +4,3 @@ def square_matrix_simple(matrix=[]):
     for arr in matrix:
         mat.append(list(map(lambda x: x*x, arr)))
     return mat
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)
