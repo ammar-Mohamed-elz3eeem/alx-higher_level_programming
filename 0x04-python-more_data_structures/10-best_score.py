3e5788dd60e643a32547ae3f1daf72e772c25631
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-12-09 21:20:15 +0200

    remove all test codes

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -11,10 +11,3 @@ def best_score(a_dictionary):
             winner = key
         i+=1
     return winner
-
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
-
-best_key = best_score(None)
-print("Best score: {}".format(best_key))
