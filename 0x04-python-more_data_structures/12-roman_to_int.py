f325fc7f080464e1e24f7f82ff2810c6df21e787
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-12-10 13:17:27 +0200

    [FIX] return 0 on error

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,11 +3,12 @@ def roman_to_int(roman_string):
     romans = {"M": 1000, "D": 500, "C": 100,
               "L": 50, "X": 10, "V": 5, "I": 1}
     number = 0
-    bigger = ""
     idx = 0
+    
+    if (roman_string is not str):
+        return 0
 
     while idx in range(0, len(roman_string)):
-
         if idx < (len(roman_string) - 1) and \
            romans[roman_string[idx]] < romans[roman_string[idx + 1]]:
             number += romans[roman_string[idx + 1]] \
