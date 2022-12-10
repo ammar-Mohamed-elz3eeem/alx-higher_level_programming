91e61c4487c5a64a495be672255f15dbc3c4cbe2
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-12-10 14:18:14 +0200

    [ADVANCED 1] avg of tuples

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    sum_weight = 0
+    sum_all = 0
+    for tup in my_list:
+        sum_weight += tup[1]
+        sum_all += tup[0] * tup[1]
+    return sum_all * 1.0 / sum_weight
