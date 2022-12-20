3208ce2e01b25681ce0daaebdfb3dd10d4c039f5
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-12-20 20:28:59 +0200

    ADD my_print function

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Define Square Class with private attribute size
+with some verfications that size attribute is correct
+"""
+
+
+class Square:
+    """ Square Class is used to create
+    squares with size == self.size """
+
+    def __init__(self, s=0):
+        self.size = s
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            if (value < 0):
+                raise ValueError("size must be >= 0")
+            self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+
+    """Function to get area of Square object"""
+    def area(self):
+        return self.__size * self.__size
+
+    """my_print: prints the square into console"""
+    def my_print(self):
+        for i1 in range(0, self.__size):
+            for i1 in range(0, self.__size):
+                print("#", end="")
+            print("")
