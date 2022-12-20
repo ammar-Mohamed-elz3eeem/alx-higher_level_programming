4dc414b1315224329695fb1bb62f8bbbaf08daae
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2022-12-20 20:02:27 +0200

    ADD private size attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Define Square Class with private attribute size
+"""
+
+
+class Square:
+    """ Square Class is used to create
+    squares with size == self.size """
+    
+    def __init__(self, s):
+       self.__size = s
