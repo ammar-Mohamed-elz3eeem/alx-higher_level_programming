38bdfa2719144b55b09ab66acf2b69f3547583a6
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-01-03 12:28:19 +0200

    fix string of error height

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -33,7 +33,7 @@ class Rectangle:
     def height(self, high):
         if isinstance(high, int):
             if high < 0:
-                raise ValueError("width must be >= 0")
+                raise ValueError("height must be >= 0")
             self.__height = high
         else:
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
