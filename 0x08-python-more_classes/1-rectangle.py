a65dcdc872d435342fd07980ef9045891848a74a
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-01-03 12:08:06 +0200

    fix properties

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,29 +9,31 @@ class Rectangle:
         This class is aimed for creating rectangles
     """
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
         return self.__width
 
     @width.setter
-    def width(self, width):
-        if (not isinstance(width, int)):
+    def width(self, wid):
+        if isinstance(wid, int):
+            if wid < 0:
+                raise ValueError("width must be >= 0")
+            self.__width = wid
+        else:
             raise TypeError("width must be an integer")
-        if (width < 0):
-            raise ValueError("width must be >= 0")
-        self.__width = width
         
     @property
     def height(self):
         return self.__height
 
     @height.setter
-    def height(self, height):
-        if (not isinstance(height, int)):
+    def height(self, high):
+        if isinstance(high, int):
+            if high < 0:
+                raise ValueError("width must be >= 0")
+            self.__height = high
+        else:
             raise TypeError("width must be an integer")
-        if (height < 0):
-            raise ValueError("width must be >= 0")
-        self.__height = height
