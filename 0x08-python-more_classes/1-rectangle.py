2212d499a68baf5fd2a7ef0fad854bcadb5ffbaa
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-01-03 11:57:03 +0200

    add height getters & setters

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,8 +8,9 @@ class Rectangle:
     """ class Rectangle:
         This class is aimed for creating rectangles
     """
-    def __init__(self, width):
+    def __init__(self, width, height):
         self.__width = width
+        self.__height = height
 
     @property
     def width(self):
@@ -22,3 +23,15 @@ class Rectangle:
         if (width < 0):
             raise ValueError("width must be >= 0")
         self.__width = width
+        
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if (not isinstance(height, int)):
+            raise TypeError("width must be an integer")
+        if (height < 0):
+            raise ValueError("width must be >= 0")
+        self.__height = height
