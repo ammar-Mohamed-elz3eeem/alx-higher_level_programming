8a04cb1850ff519006c835070ff7ec9e44fdf59b
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-01-03 11:36:47 +0200

    add docstrings

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+""" class Rectangle:
+	This class is aimed for creating rectangles
+"""
 class Rectangle:
+    """ class Rectangle:
+		This class is aimed for creating rectangles
+	"""
     pass
