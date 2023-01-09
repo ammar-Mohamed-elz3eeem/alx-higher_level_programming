c6ed597212463b7ecdebea613df915a105f009a5
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-01-09 15:49:27 +0200

    add is_same_class function

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python
+""" 
+module that contains a function to check if the type of 
+the input is same as the type specified in function or not 
+"""
+
+
+def is_same_class(obj, a_class):
+    """ return true if obj class name exactly equal to the a_class name """
+    return obj.__class__.__name__ == a_class.__name__
