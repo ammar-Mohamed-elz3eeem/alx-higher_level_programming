f83ece3d512adc053bbef2106251f2c3bb408a8a
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-01-09 18:19:28 +0200

    add __init__ method to rectangle class

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,1 +1,16 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+"""
+module that contain class Rectangle that is inherited from BaseGeometry class
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle class: inherits features from BaseGeometry class
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
