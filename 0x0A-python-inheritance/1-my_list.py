3172c4426c2284f54f5af273361dbfce73a87914
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-01-09 12:06:47 +0200

    fix pycodestyle

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,6 +2,11 @@
 """
     MyList Class: extend functionality of list object
 """
+
+
 class MyList(list):
+    """ MyList Class: extend functionality of list object
+        adding the ability to print sorted lists
+    """
     def print_sorted(self):
         print(sorted(self))
