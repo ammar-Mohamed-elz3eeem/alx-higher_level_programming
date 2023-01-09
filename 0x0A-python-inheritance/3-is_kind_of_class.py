e2dfeaa8655dc11a970d32972d4760d7cd98905e
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-01-09 15:53:50 +0200

    is kind of class function

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+"""
+module that contains a function to check if the type of
+the input is same as the type specified in function or not
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ return true if obj class name exactly equal to the a_class name """
+    return isinstance(obj, a_class)
