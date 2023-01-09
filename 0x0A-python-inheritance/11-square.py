9e35b1759a0a2a3364aaf00abeb8a967e34f92bd
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-01-09 19:46:56 +0200

    add square with print

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,1 +1,18 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+"""
+module that contain class Rectangle that is inherited from BaseGeometry class
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Rectangle class: inherits features from BaseGeometry class
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        return super().area()
