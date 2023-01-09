2d03698c732cad68d8d4ebf0e65899fc4560ef2f
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-01-09 20:03:20 +0200

    fix pycodestyle

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -3,13 +3,13 @@
 
 
 class MyInt(int):
-    """ 
-    MyInt: inherits from int class and 
-    invert the operations of equality and enequlaity operators 
     """
-    
+    MyInt: inherits from int class and
+    invert the operations of equality and enequlaity operators
+    """
+
     def __eq__(self, other):
         return not super().__eq__(other)
-    
+
     def __ne__(self, other):
         return not super().__ne__(other)
