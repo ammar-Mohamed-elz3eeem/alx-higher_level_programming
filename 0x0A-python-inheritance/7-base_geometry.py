a9c2f2444421e8e0eb88d8529b29a87836d49ebb
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-01-09 17:16:15 +0200

    fix pycodestyle

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -16,14 +16,14 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """Validate if value is integer
 
-		Args:
-			name (str): the name of the parameter
-			value (int): the value of the parameter
+        Args:
+            name (str): the name of the parameter
+            value (int): the value of the parameter
 
-		Raises:
-			TypeError: if value is not integer
-			ValueError: if value <= 0
-		"""
+        Raises:
+            TypeError: if value is not integer
+            ValueError: if value <= 0
+        """
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
