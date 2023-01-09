9288b61bf6630417fe11d15b36d90d4529bbb63a
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-01-09 16:05:51 +0200

    fix pycodestyle

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,9 +3,12 @@
 module that contains a function to check if the type of
 the input is inherited from the type specified in function or not
 """
+
+
 def inherits_from(obj, a_class):
     """
     function to check if the type of
     the input is inherited from the type specified in function or not
     """
-    return obj.__class__.__name__ != a_class.__name__ and isinstance(obj, a_class)
+    return obj.__class__.__name__ != a_class.__name__ and \
+        isinstance(obj, a_class)
