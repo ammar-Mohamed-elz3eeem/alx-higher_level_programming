89b76470c3b1b671ecb18aaf1f53e3913e90a6ff
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-01-09 16:20:50 +0200

    fix pycodestyle

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -6,12 +6,12 @@ BaseGeometry module: class BaseGeometry implementation
 
 class BaseGeometry:
     """
-    BaseGeometry Class: class with non implemented function area, 
+    BaseGeometry Class: class with non implemented function area,
     function integer_validator that validate if value is not int
     """
     def area(self):
         raise Exception("area() is not implemented")
-    
+
     def integer_validator(self, name, value):
         if value.__class__.__name__ == int:
             raise TypeError("<name> must be an integer")
