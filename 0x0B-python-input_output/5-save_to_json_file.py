d8ef3f15067effb78f2c06843eb48c0008d6bad9
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-01-13 01:36:58 +0200

    add save_to_json_file function

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Module for making save_to_json_file function """
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """save_to_json_file - convert my_obj to json
+    then writes it to filename.
+
+    Args:
+        my_obj (object): object to be converted to json.
+        filename (str): file to write the json format to.
+    """
+    with open(filename, mode="w", encoding="UTF-8") as fd:
+        json.dump(my_obj, fd)
