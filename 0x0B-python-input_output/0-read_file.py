f21796726730b0976a3a3ce2ba808e71c1537fe0
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-01-12 17:59:53 +0200

    add documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+""" Module for making read_file functions """
+
+
 def read_file(filename=""):
+    """read_file - reads the text content from the file
+
+    Args:
+        filename (str, optional): path to the file to read from it.
+        Defaults to "".
+    """
     with open(filename, mode="r", encoding="UTF-8") as fd:
         print(fd.read())
