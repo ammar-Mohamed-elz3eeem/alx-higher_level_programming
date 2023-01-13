65ed398309e9be6934e4bf89f70682031c3f507a
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-01-13 03:54:53 +0200

    imadd documentation

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
+""" module that have function for making pascal trianle """
+
+
 def pascal_triangle(n):
+
+    """ pascal_triangle - returns a list of lists of integers
+    representing the Pascal's triangle of n
+
+    Args:
+        n (int): size of pascal trinagle
+
+    Returns:
+        list: matrix to represent pascal triangle
+
+    """
 
     if n <= 0:
         return []
