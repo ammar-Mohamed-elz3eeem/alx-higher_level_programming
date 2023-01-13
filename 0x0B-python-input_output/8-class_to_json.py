b9ceaf00ada7713d7d497ae79ad50d01177f9a45
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-01-13 02:27:39 +0200

    add class_to_json function

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Module to convert any class instance to json format """
+
+
+def class_to_json(obj):
+    """convert any class instance to json format
+    
+    Args:
+        obj (object): objected to be converted to json data
+    """
+
+    return obj.__dict__
