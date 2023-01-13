2a4661e4b0bc63575c1395e11985faaa323ef555
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-01-13 02:43:45 +0200

    add to_json method to class student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" module to define a class called student """
+
+
+class Student:
+    """ Student Class: class to initialize a new object type (student) """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is not None:
+            newdict = {}
+            for attr in attrs:
+                if self.__dict__.get(attr):
+                    newdict[attr] = self.__dict__[attr]
+            return newdict
+        return self.__dict__
