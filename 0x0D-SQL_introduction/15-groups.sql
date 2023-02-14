8cbf9728c5a9b2bea73d614ba6bf3e8913537525
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-02-14 14:22:21 +0200

    order result

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- get count of same score
-SELECT score, COUNT(score) AS number from second_table GROUP BY score
+SELECT score, COUNT(score) AS number from second_table GROUP BY score ORDER BY number DESC
