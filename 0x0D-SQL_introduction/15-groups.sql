1a32a89b2bb846a77ac23b4d1b3c49c30f18a89a
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-02-14 15:07:22 +0200

    make mysql commands uppercase

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- get count of same score
-SELECT score, COUNT(score) AS number from second_table GROUP BY score ORDER BY number DESC
+SELECT score, COUNT(score) AS `number` FROM second_table GROUP BY score ORDER BY `number` DESC
