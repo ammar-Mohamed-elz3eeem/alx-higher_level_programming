f63085ec58524f11d9511e3ed688d1fb1b757f5b
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-02-17 03:26:59 +0200

    list all shows with genre or NULL

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- script that lists all shows contained in hbtn_0d_tvshows with thier genre id or null instead
+
+SELECT shows.title, genere.genre_id FROM tv_shows AS shows 
+LEFT JOIN tv_show_genres AS genere 
+ON shows.id = genere.show_id
+ORDER BY shows.title ASC, genere.genre_id ASC
