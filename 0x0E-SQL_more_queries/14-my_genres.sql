bf27076c992c519d6e8281d06a86d1a8784d9616
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-02-17 03:50:34 +0200

    genres of the show Dexter

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- lists all genres of the show Dexter
+
+SELECT tvg.name FROM tv_genres tvg 
+INNER JOIN tv_show_genres tv_g_s
+ON tv_g_s.genre_id = tvg.id
+INNER JOIN tv_shows tv_s
+ON tv_s.id = tv_g_s.show_id
+WHERE tv_s.title = 'Dexter'
+ORDER BY tvg.name ASC
