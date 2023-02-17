64610a5857e2b8684fde50e9b4ee62f8ce116aad
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-02-17 03:27:22 +0200

    add semicolon

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -3,4 +3,4 @@
 SELECT shows.title, genere.genre_id FROM tv_shows AS shows 
 LEFT JOIN tv_show_genres AS genere 
 ON shows.id = genere.show_id
-ORDER BY shows.title ASC, genere.genre_id ASC
+ORDER BY shows.title ASC, genere.genre_id ASC;
