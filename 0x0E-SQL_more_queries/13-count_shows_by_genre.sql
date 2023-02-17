ce838c1bdc603404fb9bbf7c8aafc10d4f064bcb
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-02-17 03:39:34 +0200

    add aggregation query

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,4 @@
--- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+-- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each - (aggregation query).
 
 SELECT tvg.name AS genre, COUNT(tvsg.genre_id) AS number_of_shows FROM tv_genres AS tvg
 INNER JOIN tv_show_genres AS tvsg
