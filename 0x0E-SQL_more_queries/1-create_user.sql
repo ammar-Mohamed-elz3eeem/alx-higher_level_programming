de9973c6146887577613f112eff9ddd4b7ed73a6
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-02-17 02:42:33 +0200

    set user host

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- create new user with all priviliges
 
-CREATE USER IF NOT EXISTS user_0d_1 IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL ON *.* TO user_0d_1@localhost;
