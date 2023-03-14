401b95a99733a7895f4383aea1a04055f9ab3707
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-03-14 16:32:09 +0200

    fix semistandard style

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const Rectangle = require("./4-rectangle");
+const Rectangle = require('./4-rectangle');
 module.exports = class Sqaure extends Rectangle {
-	constructor(size) {
-		super();
-		this.width = size;
-		this.height = size;
-	}
+  constructor (size) {
+    super();
+    this.width = size;
+    this.height = size;
+  }
 };
