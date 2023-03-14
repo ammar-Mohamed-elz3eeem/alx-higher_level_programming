9ef482c13647878fd94d7504a6c4e38c92027230
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-03-14 13:04:11 +0200

    add 7th & 8th script

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+if (parseInt(process.argv[2])) {
+	for (let i = 0; i < parseInt(process.argv[2]); i++) {
+		const arr = []
+		for (let j = 0; j < parseInt(process.argv[2]); j++) {
+			arr.push('X')
+		}
+		console.log(arr.join(""))
+	}
+} else {
+	console.log('Missing size')
+}
