2e03d6f4f9e9efd74e473a105d1fd2bbdbb6cbf4
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-03-14 16:48:22 +0200

    use height & width attributes

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,9 +2,9 @@
 const SQ = require('./5-square');
 module.exports = class Square extends SQ {
   charPrint (c) {
-    for (let i = 0; i < this.size; i++) {
+    for (let i = 0; i < this.height; i++) {
       let height = '';
-      for (let j = 0; j < this.size; j++) {
+      for (let j = 0; j < this.width; j++) {
         if (undefined === c) {
           height += 'X';
         } else {
