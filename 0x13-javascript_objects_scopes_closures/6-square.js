db31366fd172fd19310e17c8fed760467d3b508c
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-03-14 16:40:01 +0200

    fix super property

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,9 +2,7 @@
 const Rectangle = require('./4-rectangle');
 module.exports = class Square extends Rectangle {
   constructor (size) {
-    super();
-    this.width = size;
-    this.height = size;
+    super(size, size);
   }
 
   charPrint (c) {
