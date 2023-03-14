2bcab1f697c2ddf1e3c4b10414a83b05be3d81be
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-03-14 16:36:43 +0200

    fix class name

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const Rectangle = require('./4-rectangle');
-module.exports = class Sqaure extends Rectangle {
+module.exports = class Square extends Rectangle {
   constructor (size) {
     super();
     this.width = size;
