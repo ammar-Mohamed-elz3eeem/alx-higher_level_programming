50802c9bfde6b9ced15b72e73b3d92e7c395b3cb
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-03-14 18:40:25 +0200

    fix semistandard issues

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const dict = require("./101-data").dict;
-const newdict = Object.fromEntries(Object.entries(dict).map(el => [ el[1], [] ]));
-Object.keys(dict).map(key => newdict[dict[key]].push(key))
-console.log(newdict)
+const dict = require('./101-data').dict;
+const newdict = Object.fromEntries(Object.entries(dict).map(el => [el[1], []]));
+Object.keys(dict).map(key => newdict[dict[key]].push(key));
+console.log(newdict);
