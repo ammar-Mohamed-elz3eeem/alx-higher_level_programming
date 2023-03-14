333891eadd064296116541e1360ad956f34745aa
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-03-14 17:31:30 +0200

    fix semistandard style

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 module.exports = {
-	esrever (list) {
-		newlist = []
-		for (el of list) {
-			newlist = [el, ...newlist]
-		}
-		return newlist;
-	}
-}
+  esrever (list) {
+    let newlist = [];
+    for (const el of list) {
+      newlist = [el, ...newlist];
+    }
+    return newlist;
+  }
+};
--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,17 +1,16 @@
 #!/usr/bin/node
 
-function logger() {
-	let idx = 0;
-	return () => {
-		return idx++;
-	}
+function logger () {
+  let idx = 0;
+  return () => {
+    return idx++;
+  };
 }
 
 const loggerVar = logger();
 
 module.exports = {
-	logMe (str) {
-		let x = 0;
-		console.log(`${loggerVar()}: ${str}`);
-	}
-}
+  logMe (str) {
+    console.log(`${loggerVar()}: ${str}`);
+  }
+};
