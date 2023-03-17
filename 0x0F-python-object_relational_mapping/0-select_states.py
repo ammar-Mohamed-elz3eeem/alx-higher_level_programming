088b99b726e37ebe48f46fdaf049f9c66087b0d0
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-03-17 20:52:19 +0200

    add select states script

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+"""
+This is a module to select all states from db in ascending order
+"""
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="127.0.0.1",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    [print(row) for row in rows]
