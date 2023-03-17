7bca4a5240e29794e7080bc031f4fe820bebc594
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-03-17 21:31:39 +0200

    fix docs

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,17 +3,21 @@
 This is a module to select all states from db in ascending order
 """
 
+import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    import MySQLdb
-    import sys
 
-    db = MySQLdb.connect(host="127.0.0.1",
+    db = MySQLdb.connect(host="localhost",
                          user=sys.argv[1],
                          passwd=sys.argv[2],
                          db=sys.argv[3],
                          port=3306)
+
     cur = db.cursor()
+
     cur.execute("SELECT * FROM states ORDER BY id ASC")
+
     rows = cur.fetchall()
+
     [print(row) for row in rows]
