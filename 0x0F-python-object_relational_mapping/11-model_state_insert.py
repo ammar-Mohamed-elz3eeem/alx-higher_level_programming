8205381f60e6b2925a8e979f6d806e0e7ff00858
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-03-18 20:53:22 +0200

    add insert script

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+search for a state in the states table
+"""
+
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1],
+        argv[2],
+        argv[3]
+    ))
+    Base.metadata.create_all(bind=engine)
+    Session = sessionmaker(bind=engine)
+    sess = Session()
+    state = State(name = "Louisiana")
+    sess.add(state)
+    sess.commit()
+    
+    state_id = sess.query(State.id).filter(State.name == 'Louisiana').one()
+    print(state_id.id)
+    sess.close()
