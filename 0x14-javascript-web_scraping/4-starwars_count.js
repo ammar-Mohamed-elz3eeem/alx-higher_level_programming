869fc228e5ced420d713414d20ce622a83fbc44e
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-04-25 20:41:22 +0200

    remove forEach -> user normal for

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,20 +1,25 @@
 #!/usr/bin/node
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/';
+let times = 0;
 request(url, (err, res) => {
   if (err) {
     console.log(err);
     process.exit();
   }
-  const data = JSON.parse(res.body);
-  let times = 0;
-  data.results.forEach((val) => {
-    for (let i = 0; i < val.characters.length; i++) {
-      const charId = val.characters[i].split('/')[5];
+  const data = JSON.parse(res.body).results;
+
+  for (let i = 0; i < data.length; i++) {
+    const chars = data[i].characters;
+
+    for (let j = 0; j < chars.length; j++) {
+      const char = chars[j];
+      const charId = char.split('/')[5];
+
       if (charId === '18') {
         times++;
       }
     }
-  });
+  }
   console.log(times);
 });
