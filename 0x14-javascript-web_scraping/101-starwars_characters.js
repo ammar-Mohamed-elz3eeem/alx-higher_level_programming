dc768b34384d9898490606071009dc4649cb936f
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-04-25 22:07:13 +0200

    fix semistandard errors

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,45 +2,31 @@
 const request = require('request');
 const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 
-function get_request(url) {
+function getRequest (url) {
   return new Promise((resolve, reject) => {
     request(url, (err, response) => {
       if (err) {
         reject(err);
       }
       resolve(JSON.parse(response.body));
-    })
-  })
+    });
+  });
 }
 
-// request(url, (err, res) => {
-//   if (err) {
-//     console.log(err);
-//     process.exit();
-//   }
-//   const data = JSON.parse(res.body);
-//   const characters = data.characters;
-
-//   characters.forEach(async (charchter) => {
-//     const charInfo = await get_request(charchter);
-//     console.log(charInfo.name);
-//   });
-// });
-
-async function get_chars(url) {
-  const movieData = await get_request(url);
+async function getChars (url) {
+  const movieData = await getRequest(url);
   const charchters = movieData.characters;
   const requests = [];
   charchters.forEach(async (charchter) => {
-    const charchterInfo = get_request(charchter)
+    const charchterInfo = getRequest(charchter);
     requests.push(charchterInfo);
-  })
+  });
   const all = Promise.all(requests);
   all.then(data => {
     data.forEach(charchterInfo => {
-      console.log(charchterInfo.name)
-    })
-  })
+      console.log(charchterInfo.name);
+    });
+  });
 }
 
-get_chars(url);
+getChars(url);
